6e3edeab9897f2d1e6a5e9ca3ccdce62f41ea895
jakobsvenningsson <jaksve@kth.se>
2016-09-21 14:02:29 +0200

    Cleaned up the code, added comments, takes matrix size from user input

--- swamp.cpp
+++ swamp.cpp
@@ -4,13 +4,16 @@
 #include <vector>
 #include <list>
 #include <limits>
+#include <cstdlib>
 
+//Generates a nxn matrix, n = size. Fills it with random numbers from 1 to 9.
 void generateSwamp(std::vector<std::vector<int>> &swamp,int size){
   std::srand(std::time(0));
   for(auto &row : swamp)
     for(auto &col : row)
-      col=(std::rand()%6)+1;
+      col=(std::rand()%9)+1;
 }
+// Prints the matrix passes as argument.
 void print(std::vector<std::vector<int>> &swamp){
   for(auto row : swamp){
     for(auto col : row)
@@ -19,106 +22,98 @@ void print(std::vector<std::vector<int>> &swamp){
     }
     std::cout  << std::endl;
 }
-
+// Finds one of the cheapest left-to-right paths in the swamp matrix and returns the cheapest total trek cost.
 int findPath(std::vector<std::vector<int>> &swamp,std::vector<std::vector<int>> &shortestPath,int size){
+  // Fill out the first column with the value of the first column in swamp.
+  // This is all the possible starting poisitions.
   for(int row = 0 ; row < size ; ++row)
     shortestPath[row][0] = swamp[row][0];
+
   for(int col = 1 ; col < size ; ++col){
     for(int row = 0 ; row < size ; row++){
-      //std::cout << swamp[row][col] << " ";
       int fromSame,fromBelow,fromTop;
       fromSame = shortestPath[row][col-1];
-      if(row<(size-1)){
-        fromBelow = shortestPath[row+1][col-1];
-      }
-      else
-        fromBelow = std::numeric_limits<int>::max();
-      if(row>0)
-          fromTop = shortestPath[row-1][col-1];
-      else
-          fromTop = std::numeric_limits<int>::max();
-
-    int min = std::min(std::min(fromTop,fromBelow),fromSame);
-    shortestPath[row][col] = min + swamp[row][col];
-
+      fromBelow = row<(size-1) ? shortestPath[row+1][col-1] : std::numeric_limits<int>::max();
+      fromTop = row>0 ? shortestPath[row-1][col-1] : std::numeric_limits<int>::max();
+      int min = std::min(std::min(fromTop,fromBelow),fromSame);
+      shortestPath[row][col] = min + swamp[row][col];
     }
   }
-  //Returns the smallest total  path cost
-  int shortest=0;
+  //Returns the smallest total path cost
+  int cheapestPathCost=std::numeric_limits<int>::max();
   for(int i = 0; i < size ; i++){
-      if(shortest==0 || shortest>shortestPath[i][size-1])
-        shortest = shortestPath[i][size-1];
+      if(cheapestPathCost>shortestPath[i][size-1])
+        cheapestPathCost = shortestPath[i][size-1];
   }
-  return shortest;
+  return cheapestPathCost;
 }
 
+//Backtracks through matrices swamp and shortestPath to find the one of the cheapest left-to-right path and prints it.
 void printPath(std::vector<std::vector<int>> swamp, std::vector<std::vector<int>> shortestPath,int size){
-  std::list<int> path;
-  int start = std::numeric_limits<int>::max();
-  int index=std::numeric_limits<int>::max();
-
-  //Get the index and value of the rightmost node with the smallest value
-    for(int j = 0; j < size ; ++j){
-      if(start==0 || start>shortestPath[j][size-1]){
-        start = shortestPath[j][size-1];
-        index = j;
-      }
+  std::list<int> cheapestPath;
+  int start=std::numeric_limits<int>::max();
+  int index;
+  //Get the index and value of the rightmost node with the smallest value, i.e. the finishing node
+  // of the cheapest left-to-right path.
+  for(int j = 0; j < size ; ++j){
+    if(start>shortestPath[j][size-1]){
+      start = shortestPath[j][size-1];
+      index = j;
     }
-    path.push_front(swamp[index][size-1]);
+  }
+  cheapestPath.push_front(swamp[index][size-1]);
 
   int col = size-1;
   while(col){
-    int fromSame,fromBelow,fromTop,current;
-    if(index<size-1){
-      fromBelow = shortestPath[index+1][col-1];
-      current = swamp[index][col];
-      if(current==start-fromBelow){
-        start = fromBelow;
-        index = index+1;
-        path.push_front(swamp[index][col-1]);
-        --col;
-        continue;
-      }
-    }
-    if(index>0){
-      fromTop = shortestPath[index-1][col-1];
-      current = swamp[index][col];
-      if(current==start-fromTop){
-        index = index-1;
-        start = fromTop;
-        path.push_front(swamp[index][col-1]);
-        --col;
-        continue;
-      }
-    }
 
+    int fromSame,fromBelow,fromTop,currentNode;
+
+    currentNode = swamp[index][col];
+    fromBelow = index<(size-1) ? shortestPath[index+1][col-1] : std::numeric_limits<int>::max();
+    fromTop = index>0 ? shortestPath[index-1][col-1] : std::numeric_limits<int>::max();
     fromSame = shortestPath[index][col-1];
-    current = swamp[index][col];
-    if(current==start-fromSame){
+
+    if(currentNode==start-fromBelow){
+      start = fromBelow;
+      index +=1;
+      cheapestPath.push_front(swamp[index][col-1]);
+    }
+    else if(currentNode==start-fromTop){
+      index -=1;
+      start = fromTop;
+      cheapestPath.push_front(swamp[index][col-1]);
+    }
+    else {
       start = fromSame;
-      path.push_front(swamp[index][col-1]);
-      --col;
-      continue;
+      cheapestPath.push_front(swamp[index][col-1]);
     }
     --col;
-
   }
-  for(auto node:path)
+  //Print Cheapest Path.
+  for(auto node:cheapestPath)
     std::cout << node << " ";
   std::cout << "\n" << std::endl;
 }
 
+int main(int argc, char const *argv[]) {
 
-int main(){
-  int size = 20;
+  if(argc==1){
+    std::cout << "Enter matrix size!" << std::endl;
+    return 0;
+  }
+
+  int size = atoi(argv[1]);
   std::vector<std::vector<int>> swamp(size,std::vector<int>(size,0));
   std::vector<std::vector<int>> shortestPath(size,std::vector<int>(size,0));
+
   generateSwamp(swamp,size);
-  int x = findPath(swamp,shortestPath,size);
+  int cheapestPathCost = findPath(swamp,shortestPath,size);
+
   std::cout << "Svamp Matrix:\n"<< std::endl;
   print(swamp);
   std::cout << "Cheapest node path: ";
   printPath(swamp,shortestPath,size);
-  std::cout << "Total cost: " << x << std::endl;
+  std::cout << "Total path cost: " << cheapestPathCost << std::endl;
+
   return 0;
 }
